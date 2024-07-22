f058cd52245cb477d6d7f7dd785aecbaf9a11bba
d0dam <smallkdb@gmail.com>
2024-07-22 17:54:58 +0900

    feat: tailwind 글로벌 설정 및 폰트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const PretendardVariable = localFont({ src: './assets/fonts/PretendardVariable.woff2' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={PretendardVariable.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'button-blue': '#339afa',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
