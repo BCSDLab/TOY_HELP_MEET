34d9853f67c2952002caa1920a8d186e4ae4a213
정해성 <gotjd0230@naver.com>
2024-07-25 02:51:18 +0900

    [setting] eslint 규칙 추가 (#13)
    * feat: 의존성 추가
    
    * refactor: 의존성 수정
    
    * feat: gitignore에 항목 추가
    
    * refactor: eslintrc 수정
    
    * refactor: prettierrc bracketSameLine 설정 최신으로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import localFont from 'next/font/local';
+import '@/styles/globals.css';
 
 const PretendardVariable = localFont({ src: './assets/fonts/PretendardVariable.woff2' });
 
