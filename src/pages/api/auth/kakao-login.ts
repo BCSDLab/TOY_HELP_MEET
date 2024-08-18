0f01dc2c7fe9b6db64da362ecdfd4e8e36e20c25
정해성 <gotjd0230@naver.com>
2024-08-18 17:17:31 +0900

    refactor: early return 패턴 적용

--- src/pages/api/auth/kakao-login.ts
+++ src/pages/api/auth/kakao-login.ts
@@ -5,78 +5,78 @@ import { KakaoTokenResponse } from '@/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { code } = req.body;
-
-    try {
-      const tokenResponse = await fetch('https://kauth.kakao.com/oauth/token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
-        body: new URLSearchParams({
-          grant_type: 'authorization_code',
-          client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY!,
-          redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/callback`,
-          code: code,
-        }),
-      });
-
-      if (!tokenResponse.ok) {
-        throw new Error('Failed to fetch access token');
-      }
-
-      const tokenData: KakaoTokenResponse = await tokenResponse.json();
-
-      const userResponse = await fetch('https://kapi.kakao.com/v2/user/me', {
-        headers: {
-          Authorization: `Bearer ${tokenData.access_token}`,
-        },
-      });
-
-      const userData = await userResponse.json();
-
-      if (!userResponse.ok) {
-        throw new Error('Failed to fetch user data');
-      }
-
-      const {
-        id: kakaoId,
-        kakao_account: {
-          profile: { nickname: name, thumbnail_image_url: profileImageUrl },
-        },
-      } = userData;
-
-      const user: User = await prisma.user.upsert({
-        where: { kakaoId: String(kakaoId) },
-        update: {
-          name,
-          profileImageUrl,
-          accessToken: tokenData.access_token,
-          refreshToken: tokenData.refresh_token,
-          tokenExpiresAt: new Date(Date.now() + tokenData.expires_in * 1000),
-        },
-        create: {
-          kakaoId: String(kakaoId),
-          name,
-          profileImageUrl,
-          accessToken: tokenData.access_token,
-          refreshToken: tokenData.refresh_token,
-          tokenExpiresAt: new Date(Date.now() + tokenData.expires_in * 1000),
-        },
-      });
-
-      const jwtToken = generateToken(user.id);
-
-      res.setHeader('Set-Cookie', [
-        `auth_token=${jwtToken}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24}; SameSite=Strict`,
-      ]);
-
-      res.status(200).json({ success: true, data: user });
-    } catch (error) {
-      console.error('Error caught 500:', error);
-      res.status(500).json({ success: false, error: 'Internal server error' });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const { code } = req.body;
+
+  try {
+    const tokenResponse = await fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY!,
+        redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/callback`,
+        code: code,
+      }),
+    });
+
+    if (!tokenResponse.ok) {
+      throw new Error('Failed to fetch access token');
+    }
+
+    const tokenData: KakaoTokenResponse = await tokenResponse.json();
+
+    const userResponse = await fetch('https://kapi.kakao.com/v2/user/me', {
+      headers: {
+        Authorization: `Bearer ${tokenData.access_token}`,
+      },
+    });
+
+    if (!userResponse.ok) {
+      throw new Error('Failed to fetch user data');
+    }
+
+    const userData = await userResponse.json();
+
+    const {
+      id: kakaoId,
+      kakao_account: {
+        profile: { nickname: name, thumbnail_image_url: profileImageUrl },
+      },
+    } = userData;
+
+    const user: User = await prisma.user.upsert({
+      where: { kakaoId: String(kakaoId) },
+      update: {
+        name,
+        profileImageUrl,
+        accessToken: tokenData.access_token,
+        refreshToken: tokenData.refresh_token,
+        tokenExpiresAt: new Date(Date.now() + tokenData.expires_in * 1000),
+      },
+      create: {
+        kakaoId: String(kakaoId),
+        name,
+        profileImageUrl,
+        accessToken: tokenData.access_token,
+        refreshToken: tokenData.refresh_token,
+        tokenExpiresAt: new Date(Date.now() + tokenData.expires_in * 1000),
+      },
+    });
+
+    const jwtToken = generateToken(user.id);
+
+    res.setHeader('Set-Cookie', [
+      `auth_token=${jwtToken}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24}; SameSite=Strict`,
+    ]);
+
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    console.error('Error caught 500:', error);
+    res.status(500).json({ success: false, error: 'Internal server error' });
   }
 }
--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -3,36 +3,36 @@ import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const token = req.cookies.auth_token;
-
-    if (!token) {
-      return res.status(400).json({ success: false, message: 'No token provided' });
-    }
-
-    try {
-      const decoded = verifyToken(token);
-
-      if (decoded === null) {
-        return res.status(400).json({ success: false, message: 'Invalid token' });
-      }
-
-      await prisma.user.update({
-        where: { id: decoded.userId },
-        data: { refreshToken: null, accessToken: null, tokenExpiresAt: null },
-      });
-
-      res.setHeader('Set-Cookie', [
-        'auth_token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict',
-      ]);
-
-      res.status(200).json({ success: true, message: 'Logged out successfully' });
-    } catch (error) {
-      console.error('Logout error:', error);
-      res.status(500).json({ success: false, message: 'Internal server error' });
-    }
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const token = req.cookies.auth_token;
+
+  if (!token) {
+    return res.status(400).json({ success: false, message: 'No token provided' });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+
+    if (decoded === null) {
+      return res.status(400).json({ success: false, message: 'Invalid token' });
+    }
+
+    await prisma.user.update({
+      where: { id: decoded.userId },
+      data: { refreshToken: null, accessToken: null, tokenExpiresAt: null },
+    });
+
+    res.setHeader('Set-Cookie', [
+      'auth_token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict',
+    ]);
+
+    res.status(200).json({ success: true, message: 'Logged out successfully' });
+  } catch (error) {
+    console.error('Logout error:', error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
   }
 }
