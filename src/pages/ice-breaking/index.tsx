f5f88b7c475066f8ea57eaccee29821bc799651b
KimKyungYun <kky38225221@gmail.com>
2024-08-20 13:40:57 +0900

    feat: 아이스브레이킹 UI 제작

---
+++ src/pages/ice-breaking/index.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router';
+import Ice from '@/assets/svg/ice-breaking.svg';
+import TopNavigation from '@/components/TopNavigation';
+
+export default function IceBreaking() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center gap-10">
+      <TopNavigation />
+      <div className="mt-10 flex flex-col items-center text-3xl font-semibold">
+        아이스 브레이킹
+        <span className="text-base font-normal">모임에서 사용해보세요!</span>
+      </div>
+      <div className="mt-[10vh] flex justify-center">
+        <Ice />
+      </div>
+      <button
+        className="fixed bottom-5 w-full max-w-sm rounded-lg bg-[#3160D8] p-5 text-white cursor-pointer text-xl"
+        onClick={()=>router.push('/ice-breaking/select')}
+      >
+        시작하기
+      </button>
+    </div>
+  );
+}
