1f7fc1151d0573721651c78f5f06729104125993
KimKyungYun <kky38225221@gmail.com>
2024-08-26 17:32:45 +0900

    feat: 질문추가, 다시뽑기 기능 추가

--- src/pages/ice-breaking/select/[type]/index.tsx
+++ src/pages/ice-breaking/select/[type]/index.tsx
@@ -1,16 +1,15 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Heart from '@/assets/svg/heart.svg';
 import TopNavigation from '@/components/TopNavigation';
-import { Question, QUESTION_TYPE } from '@/pages/ice-breaking/\bentity';
+import { Question, QUESTION_LIST, QUESTION_TYPE } from '@/pages/ice-breaking/\bentity';
 
 export default function Page() {
   const router = useRouter();
-  const selected = QUESTION_TYPE[router.query.type as keyof Question] || {
-    title: '',
-    color: '',
-    shadow: '',
-  };
-
+  const questionList = QUESTION_LIST[router.query.type as keyof Question]||[];
+  const [index, setIndex] = useState(0);
+  const selected = QUESTION_TYPE[router.query.type as keyof Question] || {};
+  
   return (
     <>
       <TopNavigation path="/ice-breaking/select" close />
@@ -20,15 +19,19 @@ export default function Page() {
           카드를 선택한 당신!
         </div>
         <div
-          className="border-1 border- flex min-h-[60vh] flex-col items-center justify-center gap-3 rounded-[20px] bg-white text-2xl font-semibold"
+          className="border-1 border- flex min-h-[60vh] px-5 flex-col items-center justify-center gap-3 rounded-[20px] bg-white text-2xl font-semibold break-keep text-center"
           style={{ boxShadow: `0 0 8px 0 ${selected.shadow}` }}
         >
           <Heart style={{ fill: `${selected.color}` }} />
-          이것은 질문이여
+          {questionList[index]}
         </div>
         <button
           className="fixed bottom-3 left-[50%] h-20 w-full max-w-sm translate-x-[-50%] rounded-lg bg-[#3160D8] text-2xl text-white"
           type="button"
+          onClick={() => {
+            if (index < questionList.length-1) setIndex(index + 1);
+            else setIndex(0);        
+          }}
         >
           다시뽑기
         </button>
